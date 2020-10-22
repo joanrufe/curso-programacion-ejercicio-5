3b466b458c0bad50a5a72b461845364b24397347
Antonio Ruiz <antonio.ruiz.developer@gmail.com>
2020-10-22 23:23:39 +0200

    Metiendo tests

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import pintarEmpleados from "./pintarEmpleados";
  * Pintar listado de empleados modificando empleadosContenedor.innerHTML
  * @param {import('./empleados').EmpleadosArray} empleados
  */
-export default function iniciarApp(empleadosArray) {
-  adjuntarManejador(empleadosArray);
-  pintarEmpleados(empleadosArray);
+export default function iniciarApp(empleados) {
+  adjuntarManejador(empleados);
+  pintarEmpleados(empleados);
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,4 @@
+import { fireEvent } from "@testing-library/dom";
 import app from "./app";
 import empleados from "./empleados";
 
@@ -8,20 +9,53 @@ function mockApp() {
   </div>`;
   app(empleados);
   const searchInput = document.querySelector(".filter-employees input");
-  const searchResults = searchInput.querySelector(".filter-employees__results");
+  const searchResults = document.querySelector(".filter-employees__results");
   return {
     searchInput,
     searchResults
   };
 }
 
-test("Debe pintar el listado de empleados", () => {
-  const { searchResults } = mockApp();
+describe("Pintar empleados", () => {
+  test("Debe pintar el nombre de cada empleado", () => {
+    const { searchResults } = mockApp();
+    empleados.forEach((e) => {
+      expect(searchResults.innerHTML).toContain(e.first_name);
+    });
+  });
+
+  test("Debe pintar el apellido de cada empleado", () => {
+    const { searchResults } = mockApp();
+    empleados.forEach((e) => {
+      expect(searchResults.innerHTML).toContain(e.last_name);
+    });
+  });
 });
 
-test("Al hacer click en > cambiarán las imagenes hasta volver al principio", () => {});
+describe("Filtrar empleados", () => {
+  test("Debe filtrar por nombre", () => {
+    const { searchResults, searchInput } = mockApp();
+    const searchValue = empleados[1].first_name;
+    fireEvent.input(searchInput, { target: { value: searchValue } });
+    empleados.forEach((e) => {
+      if (e.first_name.includes(searchValue)) {
+        expect(searchResults.innerHTML).toContain(e.first_name);
+      } else {
+        expect(searchResults.innerHTML).not.toContain(e.first_name);
+      }
+    });
+  });
 
-test("Al hacer click en < cambiarán las imagenes desde el final hasta el principio", () => {
-  const { searchInput, searchResults } = mockApp();
+  test("Debe filtrar por apellido", () => {
+    const { searchResults, searchInput } = mockApp();
+    const searchValue = empleados[1].last_name;
+    fireEvent.input(searchInput, { target: { value: searchValue } });
+    empleados.forEach((e) => {
+      if (e.last_name.includes(searchValue)) {
+        expect(searchResults.innerHTML).toContain(e.last_name);
+      } else {
+        expect(searchResults.innerHTML).not.toContain(e.last_name);
+      }
+    });
+  });
 });
-// });
